8d2b88cb4e9ffa3f6c2f372776d3c34413991305
Qivex <Qivex@web.de>
2025-04-16 08:52:49 +0200

    Added support to render tiles of different zoom levels in a single draw call

--- lib/glsl/layer.vertex.glsl
+++ lib/glsl/layer.vertex.glsl
@@ -8,7 +8,7 @@ uniform vec2 center;
 uniform float zoom;
 
 in vec2 vertexPos;
-in vec2 tileOffset;
+in vec3 tileOffset;
 in float tileID;
 
 out vec2 uv;
@@ -17,6 +17,19 @@ out float sliceID;
 void main() {
 	sliceID = tileID;	// gl_InstanceID
 	uv = vertexPos;
-	vec2 clipSpace = (tilesize * (vertexPos + tileOffset) - center) / resolution * zoom;
-	gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+
+	vec2 tilePos = tileOffset.xy;
+	float layer = tileOffset.z;
+	
+	// Relative position on the grid
+	vec2 offset = tilesize * (vertexPos + tilePos);
+	// How much the entire grid is shifted ("panned")
+	vec2 shift = center * pow(2.0, layer);
+	// Size adjustment of tiles
+	float scale = pow(2.0, zoom - layer);
+
+	vec2 pixelPos = (offset - shift) * scale;
+
+	vec2 clipSpace = pixelPos * vec2(2, -2) / resolution;
+	gl_Position = vec4(clipSpace, layer / -1000.0, 1);
 }

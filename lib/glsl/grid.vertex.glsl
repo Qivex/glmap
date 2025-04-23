ca7d495556ff8f6bdd143745008c41940796ae0a
Qivex <Qivex@web.de>
2025-04-23 13:39:49 +0200

    Isolated grid drawing logic into seperate shader program (will be used to draw onto framebuffer later)

---
+++ lib/glsl/grid.vertex.glsl
@@ -0,0 +1,27 @@
+#version 300 es
+
+precision highp float;
+
+uniform vec2 gridSize;
+uniform vec2 gridCenter;
+uniform float gridZoomLevel;
+
+in vec2 vertexPos;
+in vec3 tilePos;
+
+out vec2 uv;
+
+void main() {
+	uv = vertexPos;
+
+	vec2 tileOffset = tilePos.xy;
+	float tileZoomLevel = tilePos.z;
+
+	float scale = pow(2.0, gridZoomLevel - tileZoomLevel);
+
+	vec2 gridCoordinate = (vertexPos + tileOffset - gridCenter) * scale;
+
+	vec2 clipSpace = gridCoordinate * vec2(1,-1) / gridSize;
+	
+	gl_Position = vec4(clipSpace, tileZoomLevel / -1000.0, 1);
+}
